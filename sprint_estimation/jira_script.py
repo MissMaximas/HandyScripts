27e8a01d26ae084dbda3ad3883173444922f0817
Chloe <chloe.parkes@fanduel.com>
2022-01-21 11:35:05 +0000

    Getting sprint info from Jira

--- sprint_estimation/jira_script.py
+++ sprint_estimation/jira_script.py
@@ -9,9 +9,12 @@ JIRA_CONF_PATH = "/Users/chloeparkes/missmaximas/HandyScripts/sprint_estimation/
 
 class JiraCon:
 
+    RLP1_BOARD_ID = 578
+
     def __init__(self):
         self.JIRA_CONF = JiraConfig(JIRA_CONF_PATH)
         self._conn = self._make_jira_con()
+
 
     def _make_jira_con(self):
         return JIRA(options={'server': self.JIRA_CONF.get_jira_server()},
@@ -25,5 +28,23 @@ class JiraCon:
                                  single_issue.fields.reporter.displayName)
               )
 
+    def get_project(self, project="RLP1"):
+        return self._conn.project(project)
 
-JiraCon().display_issue_details("INIT-1942")
+    def show_boards(self, project="RLP1"):
+        print(self._conn.boards(projectKeyOrID=project))
+
+    def get_latest_sprint(self):
+        sprints = self._conn.sprints(board_id=self.RLP1_BOARD_ID)
+        highest_id = 0
+        for sprint in sprints:
+            if int(sprint.id) > highest_id:
+                highest_id = int(sprint.id)
+        return self._conn.sprint_info(board_id=str(self.RLP1_BOARD_ID), sprint_id=str(highest_id))
+
+    def testy(self, issue):
+        print(self._conn.sprints(board_id=self.RLP1_BOARD_ID))
+        print(self._conn.sprints(board_id=self.RLP1_BOARD_ID))
+
+
+print(JiraCon().get_latest_sprint())
